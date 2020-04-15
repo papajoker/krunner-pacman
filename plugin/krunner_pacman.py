79a2f7eceb11ed06d393f3e79de6ece873e86e7f
papajoker <papajoke@manjaro.fr>
2020-04-15 22:03:20 +0200

    action: url or pamac

--- plugin/krunner_pacman.py
+++ plugin/krunner_pacman.py
@@ -8,11 +8,14 @@ https://api.kde.org/frameworks/krunner/html/runnercontext_8cpp_source.html#l0037
 # qdbus org.kde.krunner /App org.kde.krunner.App.querySingleRunner kpacman pacman
 
 import subprocess
+from pathlib import Path
+import webbrowser
 import dbus.service
 from dbus.mainloop.glib import DBusGMainLoop
 from gi.repository import GLib
 import pyalpm
 from pycman import config
+
 
 DBusGMainLoop(set_as_default=True)
 
@@ -24,33 +27,31 @@ class Runner(dbus.service.Object):
         dbus.service.Object.__init__(self, dbus.service.BusName("net.kpacman2", dbus.SessionBus()), objpath)
         self.alpm = None
         self.local = None
+        self.pamac = Path('/usr/bin/pamac-manager').is_file()
 
     @dbus.service.method(iface, in_signature='s', out_signature='a(sssida{sv})')
     def Match(self, query: str):
         """This method is used to get the matches and it returns a list of lists/tupels"""
         if len(query) < 3:
             return []
+        query = query.lower()
         if not self.alpm:
             self.alpm = config.init_with_config("/etc/pacman.conf")
             self.local = self.alpm.get_localdb()
+
         pkgs = []
         for repo in self.alpm.get_syncdbs():
             pkgs = pkgs + repo.search(query)
 
         ret = []
-        match = 0.1
+        match = 0.05
         for pkg in pkgs:
             match += 0.01
-            if query == pkg.name:
-                relevance = 1
-            else:
-                relevance = match
-            if self.local.get_pkg(pkg.name):
-                ico = "system-software-install"
-            else:
-                ico = ""
+            relevance = len(query) if query == pkg.name else match
+            ico = "system-software-install" if self.local.get_pkg(pkg.name) else ""
+            data = pkg.name if self.pamac else pkg.url
             ret.append((
-                pkg.name,
+                data,
                 f"{pkg.name}\t{pkg.version}",
                 ico,
                 32,    # type : https://api.kde.org/frameworks/krunner/html/runnercontext_8h_source.html
@@ -61,8 +62,11 @@ class Runner(dbus.service.Object):
 
     @dbus.service.method(iface, in_signature='ss')
     def Run(self, data: str, _action_id: str):
-        subprocess.Popen(['pamac-manager', f"--details={data}"])
-        print("run...", data, _action_id)
+        if self.pamac:
+            subprocess.Popen(['pamac-manager', f"--details={data}"])
+        else:
+            webbrowser.open_new_tab(data)
+        #print("run...", data, _action_id)
 
 
 runner = Runner()
