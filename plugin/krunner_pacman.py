4815453bb7f04ff10e1554c0abe58e5cae11d04b
papajoker <papajoke@manjaro.fr>
2020-04-16 22:17:08 +0200

    set relevance

--- plugin/krunner_pacman.py
+++ plugin/krunner_pacman.py
@@ -3,6 +3,9 @@
 """
 https://cgit.kde.org/krunner.git/plain/src/data/org.kde.krunner1.xml
 https://api.kde.org/frameworks/krunner/html/runnercontext_8cpp_source.html#l00374
+https://techbase.kde.org/Development/Tutorials/Plasma4/AbstractRunner
+
+or a framework ? https://github.com/Shihira/krunner-bridge
 """
 
 # qdbus org.kde.krunner /App org.kde.krunner.App.querySingleRunner kpacman pacman
@@ -29,25 +32,35 @@ class Runner(dbus.service.Object):
         self.local = None
         self.pamac = Path('/usr/bin/pamac-manager').is_file()
 
+    def _getpkgs(self, query: str):
+        pkgs = []
+        for repo in self.alpm.get_syncdbs():
+            pkgs = pkgs + repo.search(query)
+        return pkgs
+
     @dbus.service.method(iface, in_signature='s', out_signature='a(sssida{sv})')
     def Match(self, query: str):
-        """This method is used to get the matches and it returns a list of lists/tupels"""
+        """This method is used to get the matches and it returns a list"""
+        query = query.strip().lower()
         if len(query) < 3:
             return []
-        query = query.lower()
+
         if not self.alpm:
             self.alpm = config.init_with_config("/etc/pacman.conf")
             self.local = self.alpm.get_localdb()
 
-        pkgs = []
-        for repo in self.alpm.get_syncdbs():
-            pkgs = pkgs + repo.search(query)
+        pkgs = self._getpkgs(query)
 
         ret = []
-        match = 0.05
+        match = 0.1
         for pkg in pkgs:
-            match += 0.01
-            relevance = len(query) if query == pkg.name else match
+            relevance = match
+            if query == pkg.name:
+                relevance = 1
+            elif pkg.name.startswith(query):
+                relevance = 0.5
+            else:
+                relevance = 0.2 if query in pkg.name else match
             ico = "system-software-install" if self.local.get_pkg(pkg.name) else ""
             data = pkg.name if self.pamac else pkg.url
             ret.append((
@@ -62,6 +75,7 @@ class Runner(dbus.service.Object):
 
     @dbus.service.method(iface, in_signature='ss')
     def Run(self, data: str, _action_id: str):
+        """click on item"""
         if self.pamac:
             subprocess.Popen(['pamac-manager', f"--details={data}"])
         else:
