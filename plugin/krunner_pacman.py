c6025b20133a0381f6a70b12919a51badfc970c9
papajoker <papajoke@manjaro.fr>
2020-04-29 01:44:35 +0200

    pamac version

--- plugin/krunner_pacman.py
+++ plugin/krunner_pacman.py
@@ -15,9 +15,10 @@ from pathlib import Path
 import webbrowser
 import dbus.service
 from dbus.mainloop.glib import DBusGMainLoop
+import gi
+gi.require_version('Pamac', '9.0')
+from gi.repository import Pamac
 from gi.repository import GLib
-import pyalpm
-from pycman import config
 
 
 DBusGMainLoop(set_as_default=True)
@@ -28,15 +29,9 @@ iface = "org.kde.krunner1"
 class Runner(dbus.service.Object):
     def __init__(self):
         dbus.service.Object.__init__(self, dbus.service.BusName("net.kpacman2", dbus.SessionBus()), objpath)
-        self.alpm = None
-        self.local = None
-        self.pamac = Path('/usr/bin/pamac-manager').is_file()
-
-    def _getpkgs(self, query: str):
-        pkgs = []
-        for repo in self.alpm.get_syncdbs():
-            pkgs = pkgs + repo.search(query)
-        return pkgs
+        config = Pamac.Config(conf_path="/etc/pamac.conf")
+        config.set_enable_aur(False)
+        self.db = Pamac.Database(config=config)
 
     @dbus.service.method(iface, in_signature='s', out_signature='a(sssida{sv})')
     def Match(self, query: str):
@@ -45,44 +40,34 @@ class Runner(dbus.service.Object):
         if len(query) < 3:
             return []
 
-        if not self.alpm:
-            self.alpm = config.init_with_config("/etc/pacman.conf")
-            self.local = self.alpm.get_localdb()
-
-        pkgs = self._getpkgs(query)
-
         ret = []
         match = 0.01
-        for pkg in pkgs:
+        for pkg in self.db.search_pkgs(query):
             relevance = match
-            if query == pkg.name:
+            name = pkg.get_name()
+            if query == name:
                 relevance = 1
-            elif pkg.name.startswith(query) and not "-i18n" in pkg.name:
+            elif name.startswith(query) and not "-i18n" in name:
                 relevance = 0.4
             else:
-                relevance = 0.2 if query in pkg.name else match
-                if "-i18n" in pkg.name:
+                relevance = 0.2 if query in name else match
+                if "-i18n" in name:
                     relevance = 0.02
-            ico = "system-software-install" if self.local.get_pkg(pkg.name) else ""
-            data = pkg.name if self.pamac else pkg.url
+            ico = "system-software-install" if pkg.get_reason() else ""
             ret.append((
-                data,
-                f"{pkg.name}\t{pkg.version}",
+                name,
+                f"{name}\t{pkg.get_version()}",
                 ico,
                 32,    # type : https://api.kde.org/frameworks/krunner/html/runnercontext_8h_source.html
                 relevance,
-                {"subtext": pkg.desc, "urls": pkg.url}
+                {"subtext": pkg.get_desc()}
             ))
-        #print(ret)
         return ret
 
     @dbus.service.method(iface, in_signature='ss')
     def Run(self, data: str, _action_id: str):
         """click on item"""
-        if self.pamac:
-            subprocess.Popen(['pamac-manager', f"--details={data}"])
-        else:
-            webbrowser.open_new_tab(data)
+        subprocess.Popen(['pamac-manager', f"--details={data}"])
         #print("run...", data, _action_id)
 
 
